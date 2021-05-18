191567bbba11e47def49da3d66b2f412b6185ea7
Kane <66322897+KaneG9@users.noreply.github.com>
2021-05-18 16:04:40 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_110843) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
