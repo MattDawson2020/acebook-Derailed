d158d8b55f5c7915c4c624fa80c6da9401408cff
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-05-19 17:38:48 +0100

    password confirmation implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_19_143020) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +20,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_143020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes", default: 0
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
