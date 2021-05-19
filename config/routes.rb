eda94e2ea7b3b7bce1b8dac37d71c50847d5053d
Mike Jeuga <mikejeuga@gmail.com>
2021-05-19 11:27:36 +0100

    Kane 2 test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
-  get 'signup', to: 'users#new'
+  get "home", to: "pages#home"
+  get "signup", to: "users#new"
+  resources :sessions
+
+  post "/login", to: "sessions#create"
+  root to: "pages#home"
   resources :posts
   resources :users
 end
