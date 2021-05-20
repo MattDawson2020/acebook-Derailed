79ffa978bb9a674a9b5eb15bef4f5c3358b42f27
Mike Jeuga <mikejeuga@gmail.com>
2021-05-20 17:20:54 +0100

    fixing bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_140541) do
+ActiveRecord::Schema.define(version: 2021_05_19_143020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,14 @@ ActiveRecord::Schema.define(version: 2021_05_19_140541) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.integer "likes", default: 0
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_05_19_140541) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
   end
+
 end
