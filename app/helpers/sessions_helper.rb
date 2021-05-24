001a51697570d6426b8a3692bcf68d526fd9196b
Mike Jeuga <33467292+mikejeuga@users.noreply.github.com>
2021-05-24 12:39:50 +0100

    Update sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,2 @@
 module SessionsHelper
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-
-  # def logged_in?
-  #   !!current_user
-  # end
-
-  # def log_out
-  #   session.delete(:user_id)
-  #   @current_user = nil
-  # end
 end
