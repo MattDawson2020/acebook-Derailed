d74f30c9639d6ce1550502b4049eccb6584b8661
Kane <kgin888@gmail.com>
2021-05-25 16:29:22 +0100

    fixed

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature "Signup", type: :feature do
   scenario "User can sign up and redirected to posts" do
     visit "/"
     click_link "Sign up"
-    fill_in "Name", with: "User"
-    fill_in "Email", with: "user@gmail.com"
-    fill_in "Password", with: "123456"
-    fill_in "Password confirmation", with: "123456"
+    fill_in "signUpName", with: "User"
+    fill_in "signUpEmail", with: "user@gmail.com"
+    fill_in "signUpPassword", with: "123456"
+    fill_in "signUpPasswordConfirm", with: "123456"
     click_button "Create User"
     expect(page).to have_content("You have created your account User")
     expect(current_path).to eq("/")
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,16 +1,16 @@
 def sign_up
   visit "/"
   click_link "Sign up"
-  fill_in "Name", with: "teamDerailed"
-  fill_in "Email", with: "team@derailed.com"
-  attach_file 'Display Picture', (File.dirname(__FILE__) + '/features/images/bob_ross_img.png')
-  fill_in "Password", with: "CKMMNTT"
-  fill_in "Password confirmation", with: "CKMMNTT"
+  # save_and_open_page
+  fill_in "signUpName", with: "teamDerailed"
+  fill_in "signUpEmail", with: "team@derailed.com"
+  attach_file 'user_image', (File.dirname(__FILE__) + '/features/images/bob_ross_img.png')
+  fill_in "signUpPassword", with: "CKMMNTT"
+  fill_in "signUpPasswordConfirm", with: "CKMMNTT"
   click_button "Create User"
 end
 
 def login
-  visit "/"
   fill_in "floatingInput", with: "team@derailed.com"
   fill_in "floatingPassword", with: "CKMMNTT"
   click_button "Login"
