43e2229a363af25468c070d4d5a5728a1f31f020
Kane <kgin888@gmail.com>
2021-05-26 11:43:33 +0100

    post bar working - no styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,15 @@ class PostsController < ApplicationController
   end
 
   def create
+    p params
     @post = Post.create(message: post_params, user_id: session[:user_id])
     @post.post_image.attach(params[:post][:post_image])
     redirect_to posts_url
   end
 
   def index
+    @user = User.find(post.user_id)
+    @post = Post.new
     redirect_to "/" unless session[:user_id]
     @posts = Post.all.order(created_at: :desc)
     @comments = Comment.all
