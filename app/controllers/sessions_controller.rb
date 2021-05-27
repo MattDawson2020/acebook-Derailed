3932ded3e373265b3f9f1c50ef1ef9395bafae28
Tiago Manuel <79860571+TiagoManuelPC@users.noreply.github.com>
2021-05-27 11:22:41 +0100

    user login goes to profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to posts_path
+      redirect_to user_path(session[:user_id])
       flash[:notice] = "Successful login, welcome #{@user.name}"
     else
       redirect_to root_path
